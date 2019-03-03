6373f340b8b34f3db5c81ec6da29a5cceb8820e1
Frontendr <info@frontendr.com>
2019-03-03 16:24:14 +0100

    feat(decorator): Default the in- and exclude patterns passed from the `rendertron_render` view decorator to an empty list so all patterns will match by default for this view.

--- rendertron/decorators/django.py
+++ rendertron/decorators/django.py
@@ -9,7 +9,13 @@ def rendertron_render(**kwargs):
     to a specific view.
 
     All keyword arguments are passed to the middleware.
+    `include_patterns` and `exclude_patterns` are defaulted to an empty list
+    because it makes no sense to have it for a specific view. You can still
+    specify them for cases where your view serves responses that should be both
+    rendered and not rendered.
     """
     return decorator_from_middleware_with_args(DjangoRendertronMiddleware)(
+        include_patterns=kwargs.get('include_patterns', []),
+        exclude_patterns=kwargs.get('exclude_patterns', []),
         **kwargs
     )
