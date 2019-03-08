4d21bd0ce36d745971071bb811c12ebd281b4a71
Frontendr <info@frontendr.com>
2019-03-08 12:40:29 +0100

    tests: Added tests for included and excluded URLs

--- tests/django/test_django_views.py
+++ tests/django/test_django_views.py
@@ -41,3 +41,40 @@ class DjangoTestCase(LiveServerTestCase):
         ):
             response = self.client.get(reverse("normal_view"))
             self.assertContains(response, "Rendertron response", status_code=200)
+
+    def test_excluded_view_with_middleware(self):
+        """
+        Test whether an excluded view function is NOT rendered by the (mocked)
+        Rendertron service if the `DjangoRendertronMiddleware` is in place.
+        """
+        with self.settings(
+            # LiveServerTestCase starts a web server. self.live_server_url is it's URL.
+            RENDERTRON_BASE_URL=self.live_server_url,
+            RENDERTRON_EXCLUDE_PATTERNS=[r"^/excluded/"],
+            ROOT_URLCONF="tests.django.urls",
+            MIDDLEWARE=["rendertron.middleware.DjangoRendertronMiddleware"],
+        ):
+            response = self.client.get(reverse("excluded_view"))
+            # It should not be rendered by our rendertron view:
+            self.assertNotContains(response, "Rendertron response", status_code=200)
+            self.assertContains(response, "normal view")
+
+    def test_included_view_with_middleware(self):
+        """
+        Test whether an excluded view function is NOT rendered by the (mocked)
+        Rendertron service if the `DjangoRendertronMiddleware` is in place.
+        """
+        with self.settings(
+            # LiveServerTestCase starts a web server. self.live_server_url is it's URL.
+            RENDERTRON_BASE_URL=self.live_server_url,
+            # We exclude /excluded/
+            RENDERTRON_EXCLUDE_PATTERNS=[r"^/excluded/"],
+            # But include /excluded/included/ which should match first
+            RENDERTRON_INCLUDE_PATTERNS=[r"^/excluded/included/"],
+            ROOT_URLCONF="tests.django.urls",
+            MIDDLEWARE=["rendertron.middleware.DjangoRendertronMiddleware"],
+        ):
+            response = self.client.get(reverse("included_view"))
+            # It should not be rendered by our rendertron view:
+            self.assertContains(response, "Rendertron response", status_code=200)
+            self.assertNotContains(response, "normal view")
--- tests/django/urls.py
+++ tests/django/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     re_path("render/(?P<url>.*)$", views.rendertron_mock_render, name="mock_render"),
     # A 'normal' view, depends on middleware to be rendered by Rendertron
     path("normal/", views.normal_view, name="normal_view"),
+    # An excluded view, should never be rendered by Rendertron
+    path("excluded/", views.normal_view, name="excluded_view"),
+    # An included view, should always be rendered by Rendertron
+    path("excluded/included/", views.normal_view, name="included_view"),
     # A decorated view, should always be rendered by Rendertron
     path("decorated/", views.decorated_view, name="decorated_view"),
     # A class based view
